16563e2796ba1eb131f58dd49663ff8495e7d446
Glauco Kubrusly <gkubrusly@deloitte.com>
2025-03-24 17:51:02 +0000

    popular Model

--- webapp/controller/Produtos.controller.js
+++ webapp/controller/Produtos.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("br.com.northwind.fioriappnorthwind.controller.Produtos", {
@@ -12,7 +13,32 @@ sap.ui.define([
         },
 
         onLoad: function (value) {
-            
-        }
+            // this.getView().getModel().read("/Products", {
+            //     success: function (OData, response) {
+            //         debugger;
+            //     },
+            //     error: function (pError) {
+            //         debugger;
+            //     }
+            // });
+        },
+        onAfterRendering: function () {
+            this.getView().getModel().read("/Products", {
+                urlParameters: {
+                    "$top": 5
+                },
+                success: function (OData, response) {
+                    let oModel = new JSONModel(OData.results);
+                    this.getView().setModel(oModel, "Products");
+                    this.getView().getModel("Products").refresh();
+                    debugger;
+                }.bind(this),
+                error: (pError) => {
+                    //arrow function ja herda o this
+                    // ai, nao precisara usar o .vind(this)
+                    debugger;
+                }//.bind(this)
+            });
+        },
     });
 });
