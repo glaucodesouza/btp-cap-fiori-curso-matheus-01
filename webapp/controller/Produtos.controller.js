e5975ba15d80bfeacadea5188831c026e8d8956e
Glauco Kubrusly <gkubrusly@deloitte.com>
2025-03-24 19:37:47 +0000

    READ + Filter

--- webapp/controller/Produtos.controller.js
+++ webapp/controller/Produtos.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+], (Controller, JSONModel, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("br.com.northwind.fioriappnorthwind.controller.Produtos", {
@@ -13,32 +15,71 @@ sap.ui.define([
         },
 
         onLoad: function (value) {
-            // this.getView().getModel().read("/Products", {
-            //     success: function (OData, response) {
-            //         debugger;
-            //     },
-            //     error: function (pError) {
-            //         debugger;
-            //     }
-            // });
-        },
-        onAfterRendering: function () {
-            this.getView().getModel().read("/Products", {
+            // debugger;
+            let oFilter = new Filter({
+                filters: [
+                    new Filter("Name", FilterOperator.Contains, value)
+                ],
+                and: false
+            });
+
+            this.getView().getModel()?.read("/Products", {
                 urlParameters: {
                     "$top": 5
                 },
+                filters: [ oFilter ],
                 success: function (OData, response) {
                     let oModel = new JSONModel(OData.results);
                     this.getView().setModel(oModel, "Products");
                     this.getView().getModel("Products").refresh();
-                    debugger;
+                    // debugger;
                 }.bind(this),
                 error: (pError) => {
-                    //arrow function ja herda o this
-                    // ai, nao precisara usar o .vind(this)
-                    debugger;
+                    // debugger;
+                    // arrow function ja herda o this
+                    // ai, nao precisara usar o .bind(this)
                 }//.bind(this)
             });
         },
+        onAfterRendering: function (value) {
+
+            this.onLoad('');
+
+            // let oFilter = new Filter({
+            //     filters: [
+            //         new Filter("Name", FilterOperator.Contains, value)
+            //     ],
+            //     and: false
+            // });
+
+            // this.getView().getModel()?.read("/Products", {
+            //     urlParameters: {
+            //         "$top": 5
+            //     },
+            //     filters: [ oFilter ],
+            //     success: function (OData, response) {
+            //         let oModel = new JSONModel(OData.results);
+            //         this.getView().setModel(oModel, "Products");
+            //         this.getView().getModel("Products").refresh();
+            //     }.bind(this),
+            //     error: (pError) => {
+            //         // arrow function ja herda o this
+            //         // ai, nao precisara usar o .vind(this)
+            //     }//.bind(this)
+            // });
+        },
+
+        format: function(value){
+            try {
+                return ("R$ " + value.replace(".", ","));
+            } catch (error) {
+                return value;
+            }
+        },
+
+        onSearch: function(oEvent){
+            this.onLoad(oEvent.getSource().getValue());
+            
+        }
     });
 });
